edbbb0f85ea36affb3e506c3b2eb76db8dc36e89
Mitchell Rodio <Mitchellrodio@gmail.com>
2019-10-08 09:24:54 -0600

    final commit

--- list_quiz.rb
+++ list_quiz.rb
@@ -1,17 +1,30 @@
-# def three_even(array)
-#     array.each do |i|
-        
-        
-#     end
+def three_even(array)
+    
 
 
+#     # evenNums = (2)
+#     # if array[0] == evenNums 
+#     #     return true
+#     # else
+#     #     return false
+#     # end      
+    
+#    print array[0] # spews out random number from nowhere when use of size.times or each.do. 
+    array.each do |i|
+    if array[i] % 2 == 1 # Even num tester
+        return false
+    else
+        return true
+    end
+end
+    
 
 
-# end
+end
 
-
-# three_even([2, 1, 3, 5]) # false
-# three_even([2, 4, 12, 5]) # true
+#             0  1  2  3
+p three_even([2, 1, 3, 5]) # false
+p three_even([2, 4, 6, 8]) # true
 
 
 
@@ -30,9 +43,12 @@ def bigger_two(array1, array2)
 end
 
 p bigger_two([2, 1], [3, 4]) # [3,4]
-p bigger_two([1, 7], [4, 4]) # [1, 7]
+p bigger_two([1, 7], [4, 4]) # [1, 7] Tie
 p bigger_two([9, 9], [6, 3]) # [9, 9]
 
+# def series_up()
+
+# Don't even know how to use a number to change and add to another list.
 
 
 
